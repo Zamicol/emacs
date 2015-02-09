98b519b98c91b2f6d0bcae1c57ecef3317e3b760
Zach Collier <zachmcollier@gmail.com>
2015-02-09 11:56:55 -0700

    auto package install

--- init.el
+++ init.el
@@ -5,31 +5,61 @@
 
 
 ;;; Code:
+
+;Autoinstall packages
+;black box function stole from qbit
+(defun install-if-missing (lst)
+	"Install missing packages on init"
+		(when lst
+		(let ((pkg (car lst))
+			(rest (cdr lst)))
+			(unless (package-installed-p pkg nil)
+				(package-install pkg))
+			(install-if-missing rest))))
+
+(require 'package)
+
+;repos that we wanna load in addition to the default
+(setq package-archives 
+	'(
+		("melpa" .
+			"http://melpa.org/packages/")
+		("gnu" .
+			"http://elpa.gnu.org/packages/")
+		("marmalade" .
+			"http://marmalade-repo.org/packages/")
+		("org" .
+			"http://orgmode.org/elpa/")))
+
+(package-initialize)
+
+(unless package-archive-contents
+	(package-refresh-contents))
+
+;Make sure to include packages here if you use them below.  
+(install-if-missing
+	'(
+		flymake
+		flymake-go
+		go-mode
+		golint
+		magit
+		org
+		sr-speedbar
+	))
+
+
+
+
+;;Speedbar
 (setq 
- speedbar-use-images nil
- speedbar-show-unknown-files t
- sr-speedbar-right-side nil
-; sr-speedbar-max-width 40
- sr-speedbar-width 25
-; sr-speedbar-default-width 20
+	speedbar-use-images nil
+	speedbar-show-unknown-files t
+	sr-speedbar-right-side nil
+	sr-speedbar-width 25
+;	sr-speedbar-default-width 20
+;	sr-speedbar-max-width 40
 )
-
-;Extra Packages 
-(setq package-archives '(("melpa" .
-			  "http://melpa.org/packages/")
-			 ("gnu" .
-			  "http://elpa.gnu.org/packages/")
-			 ("marmalade" .
-			  "http://marmalade-repo.org/packages/")
-			 ("org" .
-			  "http://orgmode.org/elpa/")))
-
-(when (>= emacs-major-version 24)
-	(require 'package)
-	(package-initialize)) 
-
-;Turn off the tool bar
-(tool-bar-mode -1)
 
 ;sr-speedbar on start
 (when window-system
@@ -38,6 +68,12 @@
 ;Stop speedbar from changing size on window resize
 ;(with-current-buffer sr-speedbar-buffer-name
 ;  (setq window-size-fixed 'width))
+
+
+
+;GUI options
+;Turn off the tool bar
+(tool-bar-mode -1)
 
 
 ;custom
@@ -57,14 +93,18 @@
 ;allows to type just y instead of yes
 (defalias 'yes-or-no-p 'y-or-n-p)
 
+;Overwrite default keyboard bindings
+(global-set-key (kbd "C-x m") 'eshell)
+
+
+
+;;Coding Preferences
+
 ;flycheck
 (add-hook 'after-init-hook #'global-flycheck-mode)
 
 
-;Overwrite default keyboard bindings
-(global-set-key (kbd "C-x m") 'eshell)
-
-::this is not working as the "current buffer"? is speedbar, not the buffer where the file is being opened.  
+;;this is not working as the "current buffer"? is speedbar, not the buffer where the file is being opened.  
 (add-hook 'go-mode-hook (global-set-key (kbd "C-x f") #'gofmt))
 
 (provide 'init)
